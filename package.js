5788d555270834269563fc6f96719c978a80a50e
Stephen T Cannon <stephentcannon@gmail.com>
2014-10-30 15:21:50 -0400

    latest

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "a slightly stoopid silly logging tool for meteor",
   // Version number.
-  version: "0.0.3",
+  version: "0.0.4",
   // Optional.  Default is package directory name.
   name: "steeve:kenny-loggins",
   // Optional github URL to your source repository.
@@ -9,6 +9,8 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
+  
+  api.use('mongo@1.0.8');
   
   api.addFiles([
     'lib/Loggins.js',
